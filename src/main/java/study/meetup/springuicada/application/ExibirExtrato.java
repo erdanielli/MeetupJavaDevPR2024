2071bf690bb492c78e69b5928776c6751d53c79b
Eduardo Rebola <eduardo.rebola@libertyti.com.br>
2024-10-22 01:46:55 -0300

    use-case ExibirExtrato

---
+++ src/main/java/study/meetup/springuicada/application/ExibirExtrato.java
@@ -0,0 +1,78 @@
+package study.meetup.springuicada.application;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import study.meetup.springuicada.application.util.UseCaseOutput;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.function.Function;
+
+/// Recupera o extrato completo da conta
+public interface ExibirExtrato extends Function<ExibirExtrato.Input, ExibirExtrato.Output> {
+
+    record Input(@NotNull(message = "CONTA_REQUERIDA") @Email(message = "CONTA_INVALIDA") String conta) {
+    }
+
+    sealed interface Output extends UseCaseOutput {
+
+        record ContaDesconhecida(String nome) implements Output {
+            @Override
+            public String code() {
+                return "CONTA_DESCONHECIDA";
+            }
+        }
+
+        record Extrato(List<Operacao> extrato) implements Output {
+            @Override
+            public String code() {
+                return "EXTRATO";
+            }
+        }
+
+        sealed interface Operacao {
+            @JsonProperty
+            String tipo();
+
+            double valor();
+
+            sealed interface Efetuada extends Operacao {
+                LocalDateTime instante();
+
+                record Credito(double valor, LocalDateTime instante, String origem) implements Efetuada {
+                    @Override
+                    public String tipo() {
+                        return "CREDITO";
+                    }
+                }
+
+                record Debito(double valor, LocalDateTime instante, String destino) implements Efetuada {
+                    @Override
+                    public String tipo() {
+                        return "DEBITO";
+                    }
+                }
+            }
+
+            sealed interface Agendada extends Operacao {
+                LocalDate data();
+
+                record CreditoAgendado(double valor, LocalDate data, String origem) implements Agendada {
+                    @Override
+                    public String tipo() {
+                        return "CREDITO_AGENDADO";
+                    }
+                }
+
+                record DebitoAgendado(double valor, LocalDate data, String destino) implements Agendada {
+                    @Override
+                    public String tipo() {
+                        return "DEBITO_AGENDADO";
+                    }
+                }
+            }
+        }
+    }
+}
